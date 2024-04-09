d9e8d6be3f40acb52313ba9a4dc6f36d10d44332
Robin Shi <rsh@mega.co.nz>
2024-04-10 10:49:48 +1200

    create DefaultParserWrapper

---
+++ src/mega/privacy/android/pipeline/DefaultParserWrapper.groovy
@@ -0,0 +1,8 @@
+import org.apache.commons.cli.DefaultParser
+
+/**
+ * Create a groovy wrapper to call Apache DefaultParser.
+ */
+class DefaultParserWrapper extends DefaultParser
+        implements Serializable {
+}
