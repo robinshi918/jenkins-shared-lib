b81ee8ea609c02c0465fce4c51ab9e7d44a43a10
Robin Shi <rsh@mega.co.nz>
2024-04-11 12:09:11 +1200

    add new test methods

---
+++ src/mega/privacy/android/pipeline/MyUtil.groovy
@@ -0,0 +1,11 @@
+
+package mega.privacy.android.pipeline
+
+import java.util.Date
+
+class MyUtil {
+    String getMyHelloWorld() {
+        return "Hello World from MyUtil! ${new Date().toString()}"
+    }
+}
+
--- vars/common.groovy
+++ vars/common.groovy
@@ -4,3 +4,7 @@ import java.util.Date
 def getHelloWorld() {
     return "Hello World! ${new Date().toString()}"
 }
+
+def printSomething() {
+    println("Something")
+}
